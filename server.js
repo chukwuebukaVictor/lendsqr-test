14dc5be4ab6cc99b74a97aaa0c0ea7a60514e5c9
Chukwuebuka <chukwuebuka.victor.ozoede@gmail.com>
2022-10-06 18:45:10 +0100

    Setup eslint and install knextJS, ExpressJS and nodemon

---
+++ server.js
@@ -0,0 +1,13 @@
+// const port = process.env.PORT || 8000;
+// const server = app.listen(port, () => {
+//   console.log(`App running on port ${port}...`);
+// });
+
+// //Unhandled rejections like wrong pw in mongodp
+// process.on('unhandledRejection', (err) => {
+//   console.log(err.name, err.message);
+//   console.log('UNHANDLED REJECTION! Shutting down ...');
+//   server.close(() => {
+//     process.exit(1);
+//   });
+// });
