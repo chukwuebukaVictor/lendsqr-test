d3d3f514640494bda728e0ec2126e93cf77f63b9
Chukwuebuka <chukwuebuka.victor.ozoede@gmail.com>
2022-10-08 18:40:07 +0100

    Implement functionality for deposit

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,8 @@ const { promisify } = require('util');
 const db = require('../knex/knex');
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 dotenv.config({ path: './config.env' });
 
@@ -25,49 +27,72 @@ const createSendToken = (user, statusCode, res) => {
   });
 };
 
-exports.createUser = async (req, res, next) => {
-  try {
-    const { email, first_name, last_name } = req.body;
+exports.createUser = catchAsync(async (req, res, next) => {
+  const { email, first_name, last_name } = req.body;
 
-    if (password !== password_confirm) {
-      console.log('password and confirm password did not match');
-    }
-
-    const newUser = await db('users').insert({
-      email,
-      first_name,
-      last_name,
-      // password,
-      // password_confirm,
-    });
-    createSendToken(newUser, 201, res);
-  } catch (err) {
-    console.log(err);
+  if (password !== password_confirm) {
+    return next(
+      new AppError('password and confirm password did not match', 400)
+    );
   }
-};
 
-exports.login = async (req, res, next) => {
+  const newUser = await db('users').insert({
+    email,
+    first_name,
+    last_name,
+    // password,
+    // password_confirm,
+  });
+  createSendToken(newUser, 201, res);
+  // } catch (err) {
+  //   console.log(err);
+});
+
+exports.login = catchAsync(async (req, res, next) => {
   const { email, password } = req.body;
 
   //Check if email and password exist
   if (!email || !password) {
-    // return next(new AppError('Please provide email and password', 400));
-    console.log('Please provide email and password');
+    return next(new AppError('Please provide email and password', 400));
   }
   //Check if user exist and password is correct
   const user = (await db('users').where({ email }).select('password'))[0];
-  console.log(user);
-  console.log(password, user.password);
 
   if (!user || !(password === user.password)) {
-  // return next(new AppError('Incorrect email or password!', 401));
-  console.log('Incorrect email or password!')
+    return next(new AppError('Incorrect email or password!', 401));
   }
   //if everything is ok, send token to client
   createSendToken(user, 200, res);
+});
+
+exports.deposit = async (req, res, next) => {
+  try {
+    const user = await db('users').where({ email: req.body.email });
+    if (!user) {
+      return next(new AppError('User does not exist', 401));
+    }
+
+    const { amount } = req.body;
+    if (amount <= 0) {
+      return new error(err);
+    }
+    const newBalance = (user[0].balance += amount);
+    // await db('users').insert()
+    await db('users')
+      .where({ email: req.body.email })
+      .update('balance', newBalance);
+    res.status(200).json({
+      status: 'success',
+      user,
+    });
+  } catch (error) {}
 };
 
-exports.protect = async (req, res, next) => {
+exports.withdraw = async (req, res, next) => {
+  
+}
+
+exports.protect = catchAsync(async (req, res, next) => {
   //Get the token and check if it is there
   let token;
   if (
@@ -79,10 +104,9 @@ exports.protect = async (req, res, next) => {
   console.log(token);
 
   if (!token) {
-    // return next(
-    //   new AppError('You are not logged in! Please log in to get access', 401)
-    // );
-    console('You are not logged in! Please log in to get access');
+    return next(
+      new AppError('You are not logged in! Please log in to get access', 401)
+    );
   }
   //Token verification
   const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
@@ -90,8 +114,7 @@ exports.protect = async (req, res, next) => {
   const currentUser = await db('users').where(id, decoded.id);
   if (!currentUser) {
     return next(
-      // new AppError('The user belonging to this  token no longer exist', 401)
-      console.log('The user belonging to this  token no longer exist')
+      new AppError('The user belonging to this  token no longer exist', 401)
     );
   }
   //Check if user changed password after the token was issued
@@ -105,4 +128,4 @@ exports.protect = async (req, res, next) => {
   //Grant access to protected route
   req.user = currentUser;
   next();
-};
+});
--- routes/transactionRoute.js
+++ routes/transactionRoute.js
@@ -6,4 +6,6 @@ router.route('/').post(userController.createUser);
 
 router.route('/login').post(userController.login)
 
+router.route('/deposit').post(userController.deposit)
+
 module.exports = router;
