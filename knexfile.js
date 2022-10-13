84c274c266f840663110c60507d058730e38ff88
Chukwuebuka <chukwuebuka.victor.ozoede@gmail.com>
2022-10-13 02:37:00 +0100

    Refactor dev to production

--- knexfile.js
+++ knexfile.js
@@ -4,14 +4,28 @@ const { HOST, DB_PASSWORD } = require('./config');
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
-  development: {
-    client: 'mysql2',
-    connection: {
-      host: HOST,
-      user: 'root',
-      password: DB_PASSWORD,
-      database: 'Bank-App',
-      charset: 'utf8',
+  // development: {
+  //   client: 'mysql2',
+  //   connection: {
+  //     host: HOST,
+  //     user: 'root',
+  //     password: DB_PASSWORD,
+  //     database: 'Bank-App',
+  //     charset: 'utf8',
+  //   },
+  //   migrations: {
+  //     directory: __dirname + '/knex/migrations',
+  //   },
+  // },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
     },
     migrations: {
       directory: __dirname + '/knex/migrations',
